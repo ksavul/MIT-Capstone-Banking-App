e0238e69d29bef0269112f827806acf154160c05
ksavul <55496444+ksavul@users.noreply.github.com>
2024-01-10 19:51:49 +0100

    updated files

--- public/home.js
+++ public/home.js
@@ -1,11 +1,11 @@
-function Home(){
+function Home() {
   return (
     <Card
       txtcolor="black"
-      header="BadBank Landing Module"
-      title="Welcome to the bank"
+      header=""
+      title="Welcome to the Bad Bank!"
       text="You can move around using the navigation bar."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
     />
-  );  
+  );
 }
